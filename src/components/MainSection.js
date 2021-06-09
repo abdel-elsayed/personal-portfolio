c973f04f2982c9e801148c61d0c4edc00b01fc2c
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-06-09 14:41:10 -0400

    UI: Finished Home page udates.

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -7,113 +7,51 @@ import TitleMessage from "./TitleMessage";
 class MainSection extends Component {
   render() {
     return (
-      <div>
-        {/*<div className="main-container">
-          <div className="main-btns">
-            <div className="social-button">
-              <a
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/abdelrahman-elsayed-318539145/"
-                className="button-outline"
-                target="_blank"
-              >
-                LinkedIn <i className="fab fa-linkedin" />
-              </a>
-              <a
-                rel="noreferrer"
-                href="https://github.com/abdel-elsayed"
-                className="button-primary"
-                target="_blank"
-              >
-                GitHub <i className="fab fa-github-alt" />
-              </a>
-            </div>
-            <div className="resume-button">
-              <a
-                rel="noreferrer"
-                href="https://drive.google.com/file/d/16Mpgq_HaPl9x3s_f3Ngk2Ju9rsnQIo66/view?usp=sharing"
-                className="button-outline"
-                target="_blank"
-              >
-                Web Dev Resume <i class="fas fa-laptop-code"></i>
-              </a>
-
-              <a
-                rel="noreferrer"
-                href="https://drive.google.com/file/d/1XvAWE3iHVBvLmvJuhU66ApDrdkiZ1MSS/view?usp=sharing"
-                className="button-primary"
-                target="_blank"
-              >
-                Software Dev Resume <i class="fas fa-terminal"></i>
-              </a>
-            </div>
-          </div>
-        </div> */}
+      <div className="main-container">
         <TitleMessage />
-        <div className="main-container">
-          <div className="main-btns">
-            <div className="social-button">
-              <div className="resume-button">
-                <a
-                  rel="noreferrer"
-                  href="https://drive.google.com/file/d/16Mpgq_HaPl9x3s_f3Ngk2Ju9rsnQIo66/view?usp=sharing"
-                  className="button-primary"
-                  target="_blank"
-                >
-                  Web Dev Resume <i class="fas fa-laptop-code"></i>
-                </a>
+        <div className="left-container">
+          <div className="resume-button">
+            <a
+              rel="noreferrer"
+              href="https://drive.google.com/file/d/16Mpgq_HaPl9x3s_f3Ngk2Ju9rsnQIo66/view?usp=sharing"
+              className="button-primary first-two-buttons"
+              target="_blank"
+            >
+              Web Dev Resume <i class="fas fa-laptop-code"></i>
+            </a>
 
-                <a
-                  rel="noreferrer"
-                  href="https://drive.google.com/file/d/1XvAWE3iHVBvLmvJuhU66ApDrdkiZ1MSS/view?usp=sharing"
-                  className="button-outline"
-                  target="_blank"
-                >
-                  Software Dev Resume <i class="fas fa-terminal"></i>
-                </a>
-              </div>
-            </div>
+            <a
+              rel="noreferrer"
+              href="https://drive.google.com/file/d/1XvAWE3iHVBvLmvJuhU66ApDrdkiZ1MSS/view?usp=sharing"
+              className="button-outline first-two-buttons"
+              target="_blank"
+            >
+              Software Dev Resume <i class="fas fa-terminal"></i>
+            </a>
           </div>
-        </div>
-        <div
-          style={{
-            // marginTop: "10rem",
-            textAlign: "center",
-            marginBottom: "7rem",
-          }}
-        >
-          <p className="about-text1">
-            {/* <i class="far fa-laugh-beam"></i>. */}
-            Computer science student at City University of New York, College of
-            Staten Island. Highly motivated, passionate, and enthusiastic about
-            technology, developing and creativity.
-          </p>
-          <div className="main-container" style={{ marginBottom: "20px" }}>
-            <div className="main-btns">
-              <div className="social-button">
-                <a
-                  rel="noreferrer"
-                  href="https://www.linkedin.com/in/abdelrahman-elsayed-318539145/"
-                  className="button-primary"
-                  target="_blank"
-                >
-                  LinkedIn <i className="fab fa-linkedin" />
-                </a>
-                <a
-                  rel="noreferrer"
-                  href="https://github.com/abdel-elsayed"
-                  className="button-outline"
-                  target="_blank"
-                >
-                  GitHub <i className="fab fa-github-alt" />
-                </a>
-              </div>
-            </div>
+          <div className="social-button">
+            <a
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/abdelrahman-elsayed-318539145/"
+              className="button-primary"
+              target="_blank"
+            >
+              LinkedIn <i className="fab fa-linkedin" />
+            </a>
+            <a
+              rel="noreferrer"
+              href="https://github.com/abdel-elsayed"
+              className="button-outline"
+              target="_blank"
+            >
+              GitHub <i className="fab fa-github-alt" />
+            </a>
           </div>
-
-          <Link to="/about" className="learn-more-button ">
-            learn more <i className="fas fa-rocket"></i>
-          </Link>
+          {/* <div className="social-button">
+            <Link to="/about" className="button-outline learn-more-button">
+              learn more <i className="fas fa-rocket"></i>
+            </Link>
+          </div> */}
         </div>
       </div>
     );
