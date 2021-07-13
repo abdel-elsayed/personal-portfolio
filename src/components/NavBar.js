9913ab135aadd520c615732f9eda8f2ec645771e
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-07-13 19:14:34 -0400

    Refactored some code in Home page and removed MainSection.js/Css for simpler view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,51 +10,49 @@ function Navbar() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <div className="logo-text">A_ELSAYED</div>
-            <img style={{ width: "2em" }} alt="logo" src={logo1}></img>
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                to="/Home"
-                activeClassName="navbar__link--active"
-                className="nav-links option"
-                onClick={closeMobileMenu}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/About"
-                activeClassName="navbar__link--active"
-                className="nav-links option"
-                onClick={closeMobileMenu}
-              >
-                About me
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/Projects"
-                activeClassName="navbar__link--active"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Projects
-              </NavLink>
-            </li>
-          </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          <div className="logo-text">A_ELSAYED</div>
+          <img style={{ width: "2em" }} alt="logo" src={logo1}></img>
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
-      </nav>
-    </>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              to="/Home"
+              activeClassName="navbar__link--active"
+              className="nav-links option"
+              onClick={closeMobileMenu}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/About"
+              activeClassName="navbar__link--active"
+              className="nav-links option"
+              onClick={closeMobileMenu}
+            >
+              About me
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/Projects"
+              activeClassName="navbar__link--active"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Projects
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,59 @@
 import React from "react";
 import "../App.css";
-import MainSection from "../components/MainSection";
+import TitleMessage from "../components/TitleMessage";
+import illust from "../assets/mirage-644.png";
 import "../styling/Home.css";
 
 function Home() {
-  return <MainSection />;
+  return (
+    <div className="main-container">
+      <TitleMessage />
+      <div className="left-container">
+        <img
+          alt="waving-hand-illustration"
+          className="illustration spin"
+          src={illust}
+        />
+        <div className="resume-button">
+          <a
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/16Mpgq_HaPl9x3s_f3Ngk2Ju9rsnQIo66/view?usp=sharing"
+            className="button-primary first-two-buttons"
+            target="_blank"
+          >
+            Web Dev Resume <i class="fas fa-laptop-code"></i>
+          </a>
+
+          <a
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/1XvAWE3iHVBvLmvJuhU66ApDrdkiZ1MSS/view?usp=sharing"
+            className="button-outline first-two-buttons"
+            target="_blank"
+          >
+            Software Dev Resume <i class="fas fa-terminal"></i>
+          </a>
+        </div>
+        <div className="social-button">
+          <a
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/abdelrahman-elsayed-318539145/"
+            className="button-primary"
+            target="_blank"
+          >
+            LinkedIn <i className="fab fa-linkedin" />
+          </a>
+          <a
+            rel="noreferrer"
+            href="https://github.com/abdel-elsayed"
+            className="button-outline"
+            target="_blank"
+          >
+            GitHub <i className="fab fa-github-alt" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
